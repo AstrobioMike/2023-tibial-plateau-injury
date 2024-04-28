ca2d8154c9107305e6bf692d29021f3e41773f55
AstrobioMike <michael.lee0517@gmail.com>
2024-04-28 15:38:14 -0400

    update

--- plotting-ROM.R
+++ plotting-ROM.R
@@ -1,16 +1,21 @@
 library(tidyverse)
+library(lubridate)
 
 df <- read.table("knee-range-of-motion.tsv", sep = "\t", header = TRUE)
 
-curr_date <- format(Sys.time(), "%d-%b-%Y")
+latest_date <- format(Sys.time(), "%d-%b-%Y")
+date_vec <- df[1] %>% pull(Date)
+latest_date <- date_vec[length(date_vec)]
+latest_date <- format(dmy(latest_date), "%d-%b-%Y")
 
 ## plotting with days on X axis
 max_day_for_plot <- ceiling(df[2] %>% max() / 7) * 7
 
+### stopped making days plot after it became too crowded ###
 # with ideal deadline vertical line
 # plot_days <- ggplot(df, aes(x = Days.after.surgery, y = Degrees.bent)) + geom_point(color = "blue") +
 #     labs(y = "Degrees bent", x = "Days after surgery", title = "Knee range-of-motion recovery!",
-#          subtitle = paste0("(as of ", curr_date, ")")) +
+#          subtitle = paste0("(as of ", latest_date, ")")) +
 #     ylim(45,150) +
 #     scale_x_continuous(breaks=seq(0,max_day_for_plot,7), limits = c(0,max_day_for_plot), minor_breaks = NULL) +
 #     geom_hline(yintercept = 130, linetype = "dashed", color = "darkblue") +
@@ -24,24 +29,24 @@ max_day_for_plot <- ceiling(df[2] %>% max() / 7) * 7
 #     theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
 #     theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
 
-plot_days <- ggplot(df, aes(x = Days.after.surgery, y = Degrees.bent)) + 
-    labs(y = "Degrees bent", x = "Days after surgery", title = "Knee range-of-motion recovery!",
-         subtitle = paste0("(as of ", curr_date, ")")) +
-    scale_y_continuous(breaks=seq(50, 150, 10), limits = c(45, 150), minor_breaks = NULL) +
-    scale_x_continuous(breaks=seq(0,max_day_for_plot,7), limits = c(0,max_day_for_plot), minor_breaks = NULL) +
-    geom_hline(yintercept = 130, linetype = "dashed", color = "darkgreen") +
-    geom_hline(yintercept = 120, linetype = "dashed", color = "darkgreen") +
-    geom_text(x = 14, y = 125, label = "Nominal Goal", color = "darkgreen") +
-    annotate("rect", xmin = -Inf, xmax = Inf, ymin = 120, ymax = 130, alpha = 0.15, fill = "darkgreen") +
-    annotate("rect", xmin = -Inf, xmax = Inf, ymin = 140, ymax = Inf, alpha = 0.07, fill = "darkblue") +
-    geom_hline(yintercept = 140, linetype = "dashed", color = "darkblue") +
-    geom_text(x = 11, y = 145, label = "Better Goal", color = "darkblue") +
-    geom_point(color = "blue") +
-#     geom_segment(aes(x = 31, y = 142, xend = 31, yend = 147), color = "darkblue", lineend = "round", linejoin = "mitre", size = 0.5, 
-#         arrow = arrow(length = unit(2, "mm"))) +
-    theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+# plot_days <- ggplot(df, aes(x = Days.after.surgery, y = Degrees.bent)) + 
+#     labs(y = "Degrees bent", x = "Days after surgery", title = "Knee range-of-motion recovery!",
+#          subtitle = paste0("(as of ", latest_date, ")")) +
+#     scale_y_continuous(breaks=seq(50, 150, 10), limits = c(45, 150), minor_breaks = NULL) +
+#     scale_x_continuous(breaks=seq(0,max_day_for_plot,7), limits = c(0,max_day_for_plot), minor_breaks = NULL) +
+#     geom_hline(yintercept = 130, linetype = "dashed", color = "darkgreen") +
+#     geom_hline(yintercept = 120, linetype = "dashed", color = "darkgreen") +
+#     geom_text(x = 14, y = 125, label = "Nominal Goal", color = "darkgreen") +
+#     annotate("rect", xmin = -Inf, xmax = Inf, ymin = 120, ymax = 130, alpha = 0.15, fill = "darkgreen") +
+#     annotate("rect", xmin = -Inf, xmax = Inf, ymin = 140, ymax = Inf, alpha = 0.07, fill = "darkblue") +
+#     geom_hline(yintercept = 140, linetype = "dashed", color = "darkblue") +
+#     geom_text(x = 11, y = 145, label = "Better Goal", color = "darkblue") +
+#     geom_point(color = "blue") +
+# #     geom_segment(aes(x = 31, y = 142, xend = 31, yend = 147), color = "darkblue", lineend = "round", linejoin = "mitre", size = 0.5, 
+# #         arrow = arrow(length = unit(2, "mm"))) +
+#     theme_bw() +
+#     theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
+#     theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
 
 
 
@@ -51,7 +56,7 @@ num_total_weeks <- ceiling(df[2] %>% max() / 7)
 # with ideal deadline vertical line
 # plot_weeks <- ggplot(df, aes(x = Days.after.surgery / 7, y = Degrees.bent)) + geom_point(color = "blue") +
 #     labs(y = "Degrees bent", x = "Weeks after surgery", title = "Knee range-of-motion recovery!",
-#          subtitle = paste0("(", curr_date, ")")) +
+#          subtitle = paste0("(", latest_date, ")")) +
 #     ylim(45,150) +
 #     scale_x_continuous(breaks=seq(0, num_total_weeks, 1), limits = c(0, num_total_weeks), minor_breaks = NULL) +
 #     geom_hline(yintercept = 130, linetype = "dashed", color = "darkblue") +
@@ -64,32 +69,34 @@ num_total_weeks <- ceiling(df[2] %>% max() / 7)
 #     theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
 #     theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
 
+total_days_after_surgery_when_plot_made <- df[2] %>% max()
 plot_weeks <- ggplot(df, aes(x = Days.after.surgery / 7, y = Degrees.bent)) + 
-    labs(y = "Degrees bent", x = "Weeks after surgery", title = "Knee range-of-motion recovery!",
-        subtitle = paste0("(as of ", curr_date, ")")) +
-    scale_y_continuous(breaks=seq(50, 150, 10), limits = c(45, 150), minor_breaks = NULL) +
-    scale_x_continuous(breaks=seq(0, num_total_weeks, 2), limits = c(0, num_total_weeks), minor_breaks = NULL) +
+    labs(y = "Max degrees bent", x = "Weeks after surgery", title = "Knee range-of-motion recovery!",
+        subtitle = paste0("(as of ", latest_date, "; ", total_days_after_surgery_when_plot_made, " days after surgery)")) +
+    scale_y_continuous(breaks=seq(50, 160, 10), limits = c(45, 155), minor_breaks = NULL) +
+    scale_x_continuous(breaks=seq(0, num_total_weeks, 4), limits = c(0, num_total_weeks), minor_breaks = NULL) +
     annotate("rect", xmin = -Inf, xmax = Inf, ymin = 120, ymax = 130, alpha = 0.10, fill = "darkgreen") +
     geom_hline(yintercept = 130, linetype = "dashed", color = "darkgreen") +
     geom_hline(yintercept = 120, linetype = "dashed", color = "darkgreen") +
-    geom_text(x = 2.5, y = 125, label = "Nominal Goal", color = "darkgreen") +
+    geom_text(x = 4, y = 125, label = "Nominal Goal", color = "darkgreen") +
     annotate("rect", xmin = -Inf, xmax = Inf, ymin = 140, ymax = Inf, alpha = 0.07, fill = "darkblue") +
     geom_hline(yintercept = 140, linetype = "dashed", color = "darkblue") +
-    geom_text(x = 2, y = 145, label = "Better Goal", color = "darkblue") +
+    geom_text(x = 3, y = 151, label = "Better Goal", color = "darkblue") +
     geom_point(color = "blue") +
-    geom_segment(aes(x = 5.5, y = 142, xend = 5.5, yend = 147), color = "darkblue", lineend = "round", linejoin = "mitre", size = 0.5, 
+    geom_segment(aes(x = 8, y = 148, xend = 8, yend = 155), color = "darkblue", lineend = "round", linejoin = "mitre", size = 0.5, 
         arrow = arrow(length = unit(2, "mm"))) +
     theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
+    theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5, size = 8)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
 
 
 # writing out pdfs
-pdf(paste0(curr_date, "-knee-ROM-progress-days.pdf"), width = 5, height = 4)
-plot_days
-dev.off()
+### stopped making days plot after it became too crowded ###
+# pdf(paste0(latest_date, "-knee-ROM-progress-days.pdf"), width = 5, height = 4)
+# plot_days
+# dev.off()
 
-pdf(paste0(curr_date, "-knee-ROM-progress-weeks.pdf"), width = 5, height = 4)
+pdf(paste0(latest_date, "-knee-ROM-progress-weeks.pdf"), width = 5, height = 4)
 plot_weeks
 dev.off()
 
